d329578145841731b7dc82eb6cfbf55b21597f59
Dan Orel <mail.ordan@gmail.com>
2019-02-14 07:47:14 +0200

    Implemented the Document Distance algorithm for calculating the similarity rate. Output as a String.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,6 @@ public class App {
         FileManager.writeFile("data/doc-1.txt", "Text is approximately the same as in doc-2.txt!");
         FileManager.createFile("data/doc-2.txt");
         FileManager.writeFile("data/doc-2.txt", "Text is approximately the same as in doc-1.txt?");
-        Algorithm.getSimilarityRate(FileManager.readFile("data/doc-1.txt"), FileManager.readFile("data/doc-2.txt"));
+        System.out.println(Algorithm.getSimilarityRate(FileManager.readFile("data/doc-1.txt"), FileManager.readFile("data/doc-2.txt")));
     }
 }
--- src/main/java/algo/Algorithm.java
+++ src/main/java/algo/Algorithm.java
@@ -1,29 +1,35 @@
 package algo;
 
 import java.util.*;
-import java.util.function.BiFunction;
 
 public class Algorithm {
-    public static double getSimilarityRate(String document1, String document2){
-        return calculateVectorMultiplication(clearDocument(document1), clearDocument(document2));
+    public static String getSimilarityRate(String document1, String document2){
+        return getPercentageSimilarity(calculateSimilarityRate(clearDocument(document1), clearDocument(document2)));
     }
 
     private static String clearDocument(String content){
-        return content.replaceAll("[?,.!\"\'%$#@*()-+]", " ");
+        return content.replaceAll("[?,.!\"\'%$#@*()-+]", "");
     }
 
-    private static double calculateVectorMultiplication(String document1, String document2){
-        Hashtable<String, Integer> frequency = new Hashtable<>();
+    private static double calculateSimilarityRate(String document1, String document2){
+        Hashtable<String, Integer> firstDocumentFrequency = new Hashtable<>();
+        Hashtable<String, Integer> secondDocumentFrequency = new Hashtable<>();
         List<String> firstDocumentWords = Arrays.asList(document1.split(" "));
         List<String> secondDocumentWords = Arrays.asList(document2.split(" "));
-        Vector<String> vector = new Vector<>(firstDocumentWords);
-        vector
+        firstDocumentWords
                 .forEach(firstDocumentWord -> {
                     int counter = (int) secondDocumentWords.stream().filter(firstDocumentWord::equals).count();
-                    frequency.put(firstDocumentWord, counter);
+                    firstDocumentFrequency.put(firstDocumentWord, counter);
                 });
-        frequency
-                .forEach((key, value) -> System.out.println(key + "->" + value));
-        return 0.0;
+        double []result = {0.0};
+        for(Map.Entry<String, Integer> entry : firstDocumentFrequency.entrySet()){
+            result[0] += entry.getValue();
+        }
+        result[0] /= (firstDocumentWords.size() * secondDocumentWords.size());
+        return result[0];
+    }
+
+    private static String getPercentageSimilarity(double result){
+        return ((1 - result) * 100) + "% similarity";
     }
 }
